1877524724544fd510dd3b1a6c45303803a7a2d1
limprove <limprove89@gmail.com>
2020-04-07 23:08:39 +0900

    make it better

--- server/index.js
+++ server/index.js
@@ -53,8 +53,12 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server Running at ${port}`);
+app.listen(process.env.PORT || 8080, function() {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env,
+  );
 });
