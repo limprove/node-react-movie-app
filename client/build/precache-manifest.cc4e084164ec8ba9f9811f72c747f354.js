b491af9428bceb06906a5e7f57ddecd9b09e96ba
limprove <limprove89@gmail.com>
2020-04-07 23:04:13 +0900

    make it better

---
+++ client/build/precache-manifest.cc4e084164ec8ba9f9811f72c747f354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c16584924798c5df89173572d2eede2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "688f8eafaae0d57411b9",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "bb53c3af95907f0f2196",
+    "url": "/static/css/main.7b31cfff.chunk.css"
+  },
+  {
+    "revision": "688f8eafaae0d57411b9",
+    "url": "/static/js/2.721eed72.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/static/js/2.721eed72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb53c3af95907f0f2196",
+    "url": "/static/js/main.319fbd45.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
